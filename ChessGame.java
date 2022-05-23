37fe387318882007189e35ec91d6dadd3e7df839
dliu <dliu30@stuy.edu>
2022-05-23 10:13:28 -0400

    Made Chess Game File

---
+++ ChessGame.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class ChessGame{
+  private boolean isChecked;
+  private ArrayList<String> scoresheet;
+
+  public ChessGame(String input){
+
+  }
+
+  public static void generate(){}
+
+  public static void saveGame(){
+
+  }
+
+  public static void loadGame(){
+
+  }
+
+  public static void makeMove(){
+
+  }
+
+  public static void undoMove(){
+
+  }
+
+  public static void setStartPositions(){
+
+  }
+    //called in ChessGame
+
+  public static void showWinner(){
+
+  }
+
+  public static boolean isMated(){
+
+  }
+
+  public static boolean isTied(){
+
+  }
+}
