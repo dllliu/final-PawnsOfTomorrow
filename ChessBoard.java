1ef4e498620b072ee2f1806c3a9e115b489838bd
EugeneYoo1 <eyoo30@stuy.edu>
2022-05-23 22:09:51 -0400

    Updated constructor and getPiece methods

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,4 +1,41 @@
 public class ChessBoard{
-  public
+  private String[][] board;
+  private ArrayList<String> removedPieces;
+  private ArrayList<String> currentPieces;
+  private int color int
+  private int row;
+  private int col;
+  private String occupant;
 
+  public ChessBoard(){
+    board=new board[8][8];
+    removedPieces=new ArrayList<String>();
+    currentPieces=new ArrayList<String>();
+    
+    }
+  }
+
+  public void lastSquare(){
+
+  }
+
+  public String getSquare(String input){
+
+  }
+
+  public void flipBoard(){
+
+  }
+
+  public String toString(){
+
+  }
+
+  public void generate(){
+
+  }
+
+  public void modifyPos(){
+
+  }
 }
