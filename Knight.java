06939ab466ed82c01129c06350ccc341d8fa6165
dliu <dliu30@stuy.edu>
2022-05-25 00:14:44 -0400

    New Square class for Board, Knight implemented

--- Knight.java
+++ Knight.java
@@ -1,3 +1,15 @@
 public class Knight extends Piece{
+  public Knight(boolean isWhite){
+    super(isWhite);
+  }
 
+  public boolean canMove(board board, Square initial, Square dest){
+    if (dest.getPiece().isWhite() == this.isWhite()){
+      return false;
+    }else{
+      int x = Math.abs(initial.getX() - dest.getX()); //horizontal distance from initial to destination
+      int y = Math.abs(initial.getY() - dest.getY()); //vertical distance from initial to destination
+      return (x*y) == 2;
+    }
+}
 }
