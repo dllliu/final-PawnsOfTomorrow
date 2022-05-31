0d588aafb2d5d3af16838936d684fde1ddf058c2
dliu <dliu30@stuy.edu>
2022-05-31 19:55:04 -0400

    Piece no longer has as many methods, pawn rewritten

--- Pawn.java
+++ Pawn.java
@@ -1,10 +1,14 @@
-public class Pawn extends Piece{
-  public Pawn(String color){
-    super(color);
-  }
 
-  private int value;
-  private boolean jumpTwo;
+public class Pawn extends Piece{
+
+  public String color;
+  public boolean hasMoved;
+  public boolean emPassanAble;
+
+  public Pawn(String color){
+    this.color = color;
+    this.hasMoved = false;
+  }
 
   @Override
      public String toString(){
@@ -14,45 +18,78 @@ public class Pawn extends Piece{
 
    @Override
      public boolean canMove(Piece[][] board, int initialX, int initialY, int destX, int destY){
-       if (getColor().equals("white")) {
-         if(initialX > destX){ //cant go to left or right
+       if(color.equals("white")){
+         if(initialX > destX){
            return false;
          }
        }else{
-         if (destX > initialX){
+         if(destX > initialX){
            return false;
          }
        }
-       if (Math.abs(destX - initialX) > 2){
-         return false;
-        }
-       else if (Math.abs(destX - initialX) == 2){
-         if (hasMoved){
+
+       if(initialY == destY){
+         //Not taking a piece
+         if(color.equals("white")){
+           if(board[initialX + 1][initialY] != null){
+             return false;
+           }
+         }else{
+           if(board[initialX - 1][initialY] != null){
+             return false;
+           }
+         }
+
+         if(Math.abs(destX - initialX) > 2){
            return false;
-         }if (getColor().equals("white")) {
-           if(board[initialX + 2][initialY] != null){
-            return false;
-          }else if (board[initialX - 2][initialY] != null){
-            return false;
-          }
+         }else if(Math.abs(destX - initialX) == 2){
+           //jump Two
+           if(hasMoved){
+             return false;
+           }
+
+           if(color.equals("white")){
+             if(board[initialX + 2][initialY] != null){
+               return false;
+             }
+           }else{
+             if(board[initialX - 2][initialY] != null){
+               return false;
+             }
+           }
+
+           //En passante
+           if(destY + 1 < 8){
+             if(board[destX][destY + 1] != null){
+               if(board[destX][destY + 1].getClass().isInstance(new Pawn("white"))){
+                 emPassanAble = true;
+               }
+             }
+           }else if(destY - 1 > 0){
+             if(board[destX][destY - 1] != null){
+               if(board[destX][destY - 1].getClass().isInstance(new Pawn("white"))){
+                 emPassanAble = true;
+               }
+             }
+           }
+
          }
        }else{
-         if (Math.abs(destY - initialY) != 1 || Math.abs(destX - initialX) != 1){
+         //Capturing
+         if(Math.abs(destY - initialY) != 1 || Math.abs(destX - initialX) != 1){
            return false;
-         } if (board[destX][destY] == null){
+         }
+
+         if(board[destX][destY] == null){
            return false;
          }
        }
-       if (Math.abs(destX-initialX)==1&&Math.abs(destY-destX)==1){
-         if (board[initialX-2][initialY]!=board[initialX-1][initialY]||board[initialX][initialY]!= board[initialX-1][initialY]){
-           return true;
-         }
-       }
+
        return true;
      }
 
      public String getColor(){
-		return this.color;
-	}
+        return this.color;
+    }
 
    }
--- Piece.java
+++ Piece.java
@@ -1,27 +1,11 @@
-
 public abstract class Piece{
 
-  private boolean captured = false;
   public boolean hasMoved;
-  public boolean emPassanAble;
-  private ChessBoard board;
   public String color;
 
 
-  public Piece(String color){
-    this.color = color;
-  }
-
   public String getColor(){
     return this.color;
-  }
-
-  public boolean isCaptured(){
-    return this.captured;
-  }
-
-  public void setCaptured(boolean killed){
-     this.captured = captured;
   }
 
   public abstract boolean canMove(Piece[][] board, int initialX, int initialY, int destX, int destY);
