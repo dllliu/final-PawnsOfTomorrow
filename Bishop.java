edcde0c1eceee30458166aa46b48e7ce9498953d
dliu <dliu30@stuy.edu>
2022-06-04 12:58:45 -0400

    Fixed bishop movement

--- Bishop.java
+++ Bishop.java
@@ -12,7 +12,7 @@ public class Bishop extends Piece{
   @Override
   public boolean canMove(ChessBoard cb, int initialX, int initialY, int destX, int destY){
     Piece[][] board=cb.board;
-        if ((initialY == destY && initialX == destX)) {
+        if ((initialY == destY || initialX == destX)) {
       return false;
     }else if (Math.abs(initialY - destY) != (Math.abs(initialX - destX))) {
       return false;
@@ -29,7 +29,7 @@ public class Bishop extends Piece{
     }else{
       colInc = -1;
     }
-    int totalInc = rowInc + colInc;
+    int totalInc = initialY + colInc;
     for(int x= initialX + rowInc; x != destX; x+= rowInc){
       if(board[x][totalInc] != null) {
         return false;
