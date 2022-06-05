da73c9541e359e77c3302c4b55fdc16c146a7c56
EugeneYoo1 <eyoo30@stuy.edu>
2022-06-05 01:22:11 -0400

    Added in preventing castling through check

--- King.java
+++ King.java
@@ -29,7 +29,8 @@ public class King extends Piece{
 		return this.color;
 	}
 
-
+//need to code white king black king are 1 unit away from each other
+//lichess puzzles
 @Override
 public boolean canMove(ChessBoard cb, int initialX, int initialY, int destX, int destY){
   Piece[][] board=cb.board;
@@ -38,18 +39,36 @@ public boolean canMove(ChessBoard cb, int initialX, int initialY, int destX, int
     if(hasMoved){
       return false;
     }
-
     if(destY - initialY == 2 && initialX == destX){
       //Castle king side
+
       if(board[destX][initialY + 1] != null || board[destX][initialY + 2] != null){
         hasCastled = false;
         return false;
+      }
+      for(int i = 0; i<board.length; i++){
+        for(int j = 0; j<board[0].length; j++){
+          if(board[i][j] != null&&!board[i][j].getColor().equals(color)&&!(board[i][j].getClass().isInstance(new King((color))))){
+            if(board[i][j].canMove(cb, i, j, initialX, initialY) || board[i][j].canMove(cb, i, j, initialX, initialY+1)||board[i][j].canMove(cb, i, j, initialX, initialY+2)){
+              return false;
+            }
+          }
+        }
       }
       //Castle Queen side
     }else if(initialY - destY == 2 && initialX == destX && (destY < initialY)){
       if(board[destX][initialY - 1] != null || board[destX][initialY - 2] != null || board[destX][initialY - 3] != null){
         hasCastled = false;
         return false;
+      }
+      for(int i = 0; i<board.length; i++){
+        for(int j = 0; j<board[0].length; j++){
+          if(board[i][j] != null&&!board[i][j].getColor().equals(color)&&!(board[i][j].getClass().isInstance(new King((color))))){
+            if((board[i][j].canMove(cb, i, j, initialX, initialY) || board[i][j].canMove(cb, i, j, initialX, initialY-1)||board[i][j].canMove(cb, i, j, initialX, initialY-2))){
+              return false;
+            }
+          }
+        }
       }
     }else{
       hasCastled = false;
