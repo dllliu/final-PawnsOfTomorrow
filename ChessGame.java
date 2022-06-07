c9eca2d2e8e3a399658f852cdd40c9c32904f761
EugeneYoo1 <eyoo30@stuy.edu>
2022-06-07 00:10:50 -0400

    Made minor changes to all files, such as allowing ChessPuzzle and ChessGame to print clear boards everytime they are called.

--- ChessGame.java
+++ ChessGame.java
@@ -111,50 +111,48 @@ public static void main(String[] args) {
   ChessBoard newBoard = new ChessBoard(locationList,piecesList,colorList);
   boolean possibleStalemate = false;
   String color = "white";
+  System.out.print("\033[H\033[2J");
+  System.out.flush();
+  System.out.println(newBoard);
+  int count=0;
   while(true){
     System.out.println (newBoard.scoreSheet.toString());
-    System.out.println(newBoard);
     System.out.println(color + " enter your move");
     Scanner in = new Scanner(System.in);
     String move = in.nextLine();
     if (move.contains("resign")){
-      System.out.println(color + " reigns");
+      System.out.println(color + " resigns");
       System.out.println(otherColor(color) + " wins");
       return;
-    }if(possibleStalemate){
-      if(move.toLowerCase().contains("draw") || move.toLowerCase().contains("stalemate")){
-        System.out.println("The game has ended in a draw");
-        return;
-      }else{
-        possibleStalemate = false;
-      }
-    } try{
+    }
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+    try{
       newBoard.makeMove(move, color, true);
+      System.out.println(newBoard);
     }catch(IllegalArgumentException e){
       e.printStackTrace();
       System.out.println("Invalid move: Enter Moves in following format: Character + number + space + Character + number");
+      System.out.println(newBoard);
       continue;
     }
-
-//?
-    Piece[][] oldBoard = newBoard.board.clone();
     if(!newBoard.canAnyMove(otherColor(color))){
-      if(!newBoard.canAnyMove(otherColor(color))){
+      if(newBoard.isChecked(otherColor(color))){
         System.out.println(color + " checkmated " + otherColor(color));
-    }
+      }
     else{
       System.out.println("Game has ended in a stalemate");
     }
     return;
-}
-    newBoard.board = oldBoard;
-    //point?
+  }
    if(newBoard.isChecked(otherColor(color))) {
       System.out.println(otherColor(color) + " is in check.");
-    } if(move.contains("draw")){
-        possibleStalemate = true;
     }
-      color = otherColor(color);
+    color = otherColor(color);
+    if (newBoard.count50==100){
+      System.out.println("Game has ended in a draw due to the 50 move rule");
+      return;
+    }
   }
 }
 
