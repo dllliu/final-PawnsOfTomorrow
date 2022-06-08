b95e505e6eb35d3be92b57ab25e7b116aac64959
EugeneYoo1 <eyoo30@stuy.edu>
2022-06-08 15:17:43 -0400

    Cleaned code and removed compile errors caused by merge conflict

--- ChessGame.java
+++ ChessGame.java
@@ -10,7 +10,6 @@ public static void main(String[] args) {
   System.out.println("Enter which mode you want to play. 1 is 3-Check, 2 is King of the Hill, 3 is Atomic Chess, 4 is Chess 960. \n Just run the file if you want to play normal chess.");
   System.out.println("If you want to solve chess tactics, enter in puzzle1 and so on...");
  //String tutorialMode = args[1];
- System.out.print()
  if(args.length == 0){
 
    ArrayList<String> locationList = new ArrayList<String>();
@@ -270,7 +269,6 @@ public static void main(String[] args) {
   colorList.add("black");
 
   ChessBoard newBoard = new ChessBoard(locationList,piecesList,colorList);
-<<<<<<< HEAD
   if(chessMode == 1){
     int whiteCheckCount = 0;
     int blackCheckCount = 0;
