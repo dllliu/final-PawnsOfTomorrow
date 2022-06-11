288b31b0998bc16cc391f21a6863e267f2cf064d
EugeneYoo1 <eyoo30@stuy.edu>
2022-06-11 10:36:07 -0400

    Added in 3 more important demo cases

--- Demo.java
+++ Demo.java
@@ -6,6 +6,78 @@ public class Demo {
       ArrayList<String> colorList = new ArrayList<String>();
       ArrayList<String> solution= new ArrayList<String>();
 
+      solution.add("h2 h4");
+      solution.add("a8 b8");
+      solution.add("h4 h5");
+      solution.add("g7 g5");
+      solution.add("h5 g6");
+
+      locationList.add("66");
+      colorList.add("black");
+      piecesList.add("pawn");
+      locationList.add("71");
+      colorList.add("white");
+      piecesList.add("pawn");
+      locationList.add("07");
+      colorList.add("black");
+      piecesList.add("king");
+      locationList.add("00");
+      colorList.add("white");
+      piecesList.add("king");
+      ChessPuzzle.solve("enpassant, which is when a pawn can capture an opponent pawn if that pawn has moved immediately two squares and is directly adjacent to your pawn",locationList,piecesList,colorList,solution,"white",true);
+      solution.clear();
+      colorList.clear();
+      piecesList.clear();
+      locationList.clear();
+
+      solution.add("h3 h4");
+      locationList.add("71");
+      colorList.add("white");
+      piecesList.add("pawn");
+      locationList.add("54");
+      colorList.add("black");
+      piecesList.add("king");
+      locationList.add("00");
+      colorList.add("white");
+      piecesList.add("king");
+      ChessPuzzle.solve("invalid move due to invalid start square",locationList,piecesList,colorList,solution,"white",true);
+      solution.clear();
+      colorList.clear();
+      piecesList.clear();
+      locationList.clear();
+
+      solution.add("h2 h5");
+      locationList.add("71");
+      colorList.add("white");
+      piecesList.add("pawn");
+      locationList.add("54");
+      colorList.add("black");
+      piecesList.add("king");
+      locationList.add("00");
+      colorList.add("white");
+      piecesList.add("king");
+      ChessPuzzle.solve("invalid move due to invalid destination square",locationList,piecesList,colorList,solution,"white",true);
+      solution.clear();
+      colorList.clear();
+      piecesList.clear();
+      locationList.clear();
+
+      solution.add("h3 h4");
+      locationList.add("71");
+      colorList.add("white");
+      piecesList.add("pawn");
+      locationList.add("54");
+      colorList.add("black");
+      piecesList.add("king");
+      locationList.add("00");
+      colorList.add("white");
+      piecesList.add("king");
+      ChessPuzzle.solve("invalid move due to invalid start and destination square",locationList,piecesList,colorList,solution,"white",true);
+      solution.clear();
+      colorList.clear();
+      piecesList.clear();
+      locationList.clear();
+
       solution.add("h7 h8 N");
       locationList.add("76");
       colorList.add("white");
