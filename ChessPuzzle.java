2a431a08d9cd0179fd0cda41a2dd4f610f918ac3
EugeneYoo1 <eyoo30@stuy.edu>
2022-06-11 23:35:47 -0400

    Made changes so that error messages are printed in demo mode. I also made invalid moves not stop the program, and it also can print out true statements, such as if it is a stalemate position, or if someone is in check

--- ChessPuzzle.java
+++ ChessPuzzle.java
@@ -24,30 +24,48 @@ public class ChessPuzzle{
         System.out.println(otherColor(color) + " wins");
         return;
       }
-      if(!newBoard.canAnyMove(otherColor(color))){
-        if(newBoard.isChecked(otherColor(color))){
-          System.out.println(color + " checkmated " + otherColor(color));
-        }
-      else{
-        System.out.println("Game has ended in a stalemate");
-      }
-      return;
-    }
-     if(newBoard.isChecked(otherColor(color))) {
-        System.out.println(otherColor(color) + " is in check.");
-      }
-      if (newBoard.count50==100){
-        System.out.println("Game has ended in a draw due to the 50 move rule");
-        return;
-      }
       if(!move.equals(solution.get(0))){
         System.out.print("\033[H\033[2J");
         System.out.flush();
+        try{
+          Piece[][] oldBoard=new Piece[newBoard.board.length][newBoard.board.length];
+          for (int i=0;i<newBoard.board.length;i++){
+            for (int k=0;k<newBoard.board.length;k++){
+              oldBoard[i][k]=newBoard.board[i][k];
+            }
+          }
+          newBoard.makeMove(move, color, false);
+          for (int m=0;m<newBoard.board.length;m++){
+            for (int n=0;n<newBoard.board.length;n++){
+              newBoard.board[m][n]=oldBoard[m][n];
+            }
+          }
          System.out.println("Wrong move. Try again");
+       }
+        catch(IllegalArgumentException e){
+          e.printStackTrace();
+          System.out.println("Invalid move: Enter Moves in following format: Character + number + space + Character + number");
+          continue;
+        }
         continue;
       }
       else{
         newBoard.makeMove(move, color, true);
+          if(!newBoard.canAnyMove(otherColor(color))){
+            if(newBoard.isChecked(otherColor(color))){
+              System.out.println(color + " checkmated " + otherColor(color));
+            }
+          else{
+            System.out.println("Game has ended in a stalemate");
+          }
+        }
+         if(newBoard.isChecked(otherColor(color))) {
+            System.out.println(otherColor(color) + " is in check.");
+          }
+          if (newBoard.count50==100){
+            System.out.println("Game has ended in a draw due to the 50 move rule");
+          }
+
         solution.remove(0);
         System.out.print("\033[H\033[2J");
         System.out.flush();
